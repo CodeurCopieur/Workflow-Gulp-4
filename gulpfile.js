aed2f02349189dd9e1d7d8c1a49f363f319d93b0
codwerk <louis_widdy@live.fr>
2019-06-07 23:47:54 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 'use strict';
+
+// Include plugins
 const gulp = require('gulp'),
-      gp = require('gulp-load-plugins')();
+      gp = require('gulp-load-plugins')(); // tous les plugins de package.json
+const browserSync = require('browser-sync').create();
 
-
+// Configuration de base
 const paths = {
       html : {
           src : './src/pug/pages/*.pug',
-          dest: './build/html'
+          dest: './html'
       },
       css: {
           src : './src/static/sass/*.scss',
@@ -14,12 +17,22 @@ const paths = {
       }
 }
 
+// Static server
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+});
+
 const pug = () =>
       gulp.src(paths.html.src)
           .pipe(gp.pug({
               pretty: true
           }))
-          .pipe(gulp.dest(paths.html.dest));
+          .pipe(gulp.dest(paths.html.dest))
+          .on('end',browserSync.reload);
 
 const sass = () =>
       gulp.src(paths.css.src)
@@ -38,7 +51,8 @@ const sass = () =>
            .pipe(gp.cssbeautify({indent: ' '}))
            .pipe(gp.csso())
            .pipe(gp.sourcemaps.write())
-           .pipe(gulp.dest(paths.css.dest));  
+           .pipe(gulp.dest(paths.css.dest))
+           .on('end',browserSync.reload);  
 
 
 const watch = () =>
